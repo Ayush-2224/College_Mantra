06de62ae958942b799e3cccc6bc38d0c7fe9c290
AYUSH KUMAR <154232373+Ayush-2224@users.noreply.github.com>
2025-06-28 16:14:15 +0530

    Update urls.py

--- cm_app/urls.py
+++ cm_app/urls.py
@@ -1,20 +1,8 @@
 
-# from django.urls import path
-# from . import views
-# app_name = 'cm_app'
-# urlpatterns = [
-#     # Home route (if defined in cm_app)
-#     path('', views.home, name='home'),  # This is redundant if already included in cm_app.urls
-#     path('about/', views.about, name='about'),
-#     path('contact/', views.contact, name='contact'),
-#     path('register/', views.option, name='register'),
-#     path('seat_matrix/', views.seat_matrix, name='about'),
-
-# ]
 from django.urls import path
 from . import views
 
-app_name = 'cm_app'  # This sets the namespace for your app
+app_name = 'cm_app'  
 
 urlpatterns = [
     path('', views.home, name='home'),
